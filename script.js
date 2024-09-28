55aa507bb9b67748606e1e876a9ccf0914756381
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2024-09-28 20:16:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+let element = document.querySelector("#level")
+
+let count = 1
+
+while(element.parentElement){
+	count++
+	element = element.parentElement
+}
+
+alert(parseInt(count))
